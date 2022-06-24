3a2f35cc5982b2317ebc3cee5358cf235a57675a
DavidPetkovsek <david.petkovsek@martinrea.com>
2022-06-24 16:19:46 -0400

    convert to CMake lib

--- include/Converter.h
+++ include/Converter.h
@@ -23,11 +23,11 @@
 #include<opencv2/core/core.hpp>
 
 #include<Eigen/Dense>
-#include"Thirdparty/g2o/g2o/types/types_six_dof_expmap.h"
-#include"Thirdparty/g2o/g2o/types/types_seven_dof_expmap.h"
+#include"g2o/types/types_six_dof_expmap.h"
+#include"g2o/types/types_seven_dof_expmap.h"
 
-#include "Thirdparty/Sophus/sophus/geometry.hpp"
-#include "Thirdparty/Sophus/sophus/sim3.hpp"
+#include "sophus/geometry.hpp"
+#include "sophus/sim3.hpp"
 
 namespace ORB_SLAM3
 {
