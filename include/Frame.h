8065b05bd9932b0049c19e51c9d18828c15f2a6e
DavidPetkovsek <david.petkovsek@martinrea.com>
2022-06-24 16:19:46 -0400

    convert to CMake lib

--- include/Frame.h
+++ include/Frame.h
@@ -22,10 +22,10 @@
 
 #include<vector>
 
-#include "Thirdparty/DBoW2/DBoW2/BowVector.h"
-#include "Thirdparty/DBoW2/DBoW2/FeatureVector.h"
+#include "DBoW2/BowVector.h"
+#include "DBoW2/FeatureVector.h"
 
-#include "Thirdparty/Sophus/sophus/geometry.hpp"
+#include "sophus/geometry.hpp"
 
 #include "ImuTypes.h"
 #include "ORBVocabulary.h"
--- include/LoopClosing.h
+++ include/LoopClosing.h
@@ -31,7 +31,7 @@
 #include <boost/algorithm/string.hpp>
 #include <thread>
 #include <mutex>
-#include "Thirdparty/g2o/g2o/types/types_seven_dof_expmap.h"
+#include "g2o/types/types_seven_dof_expmap.h"
 
 namespace ORB_SLAM3
 {
